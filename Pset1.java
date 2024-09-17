e83931977dbdddb0828b2bde21258fb9c7d39a6f
andrewpham23 <112991039+andrewpham23@users.noreply.github.com>
2024-09-17 13:22:45 -0700

    test

---
+++ Pset1.java
@@ -0,0 +1,16 @@
+class Pset1 {
+	public static void main(String[] args) {
+		
+
+
+		System.out.print("question 1 answer: Andrew Pham is a student in IB CS ");
+		System.out.print("question 2a answer: Java is a compiled language whereas python intepreted langayuge ");
+		System.out.print("question 2b answer: assembly Languages are lower level langauges, that translate high level languages into machine langauges. Higher level languages are used by programmers to write most programs ");
+		System.out.print("question 2c answer: the compiler translaters a progam wrote in a higher level language into machine code before the code is ran ");
+	}
+
+
+
+
+
+}
